c01437e24801bab6105da9c83ef955b8d4960b1c
PeerawatProject14 <peerawatwork2024@email.com>
2024-08-11 18:12:15 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Calender from './class-components'
+
+export default function App(){
+  return(
+    <>
+      <p><center><Calender></Calender></center></p>
+    </>
+  )
+}
---
+++ src/class-components.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Calendar extends React.Component {
+    getDate() {
+        const dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+        const monthNames = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+        const date = new Date();
+        const weekDay = dayNames[date.getDay()];
+        const day = date.getDate();
+        const month = monthNames[date.getMonth()];
+        const year = date.getFullYear();
+        return `${weekDay} ${day} ${month} ${year}`;
+    }
+    
+    render() {
+        return <div>{this.getDate()}</div>;
+    }
+}
